52bfd6c0911f7c4a4b965666518aa99e5185a1eb
joma74 <dev-mails@gmx.net>
2018-06-19 00:12:10 +0200

    chore(refactor): make team advised code improvements

--- lib/templateCompilerModules/utils.ts
+++ lib/templateCompilerModules/utils.ts
@@ -10,8 +10,8 @@ export interface ASTNode {
 
 import { UrlWithStringQuery, parse as uriParse } from 'url'
 
-export function urlToRequire(url: string): string {
-  let returnValue = `"${url}"`
+export function urlToRequire (url: string): string {
+  const returnValue = `"${url}"`
   // same logic as in transform-require.js
   const firstChar = url.charAt(0)
   if (firstChar === '.' || firstChar === '~' || firstChar === '@') {
@@ -22,13 +22,15 @@ export function urlToRequire(url: string): string {
 
     const uriParts = parseUriParts(url)
 
-    !uriParts.hash
-      ? (returnValue = `require("${url}")`)
-      : // support uri fragment case by excluding it from
-        // the require and instead appending it as string;
-        // assuming that the path part is sufficient according to
-        // the above caseing(t.i. no protocol-auth-host parts expected)
-        (returnValue = `require("${uriParts.path}") + "${uriParts.hash}"`)
+    if(!uriParts.hash) {
+      return `require("${url}")`
+    } else{
+      // support uri fragment case by excluding it from
+      // the require and instead appending it as string; 
+      // assuming that the path part is sufficient according to 
+      // the above caseing(t.i. no protocol-auth-host parts expected)
+      return `require("${uriParts.path}") + "${uriParts.hash}"`
+    }
   }
   return returnValue
 }
@@ -37,14 +39,16 @@ export function urlToRequire(url: string): string {
  * vuejs/component-compiler-utils#22 Support uri fragment in transformed require
  * @param urlString an url as a string
  */
-function parseUriParts(urlString: string): UrlWithStringQuery {
-  // initialize return value
-  const returnValue: UrlWithStringQuery = uriParse('')
-  if (urlString) {
-    // A TypeError is thrown if urlString is not a string
-    // @see https://nodejs.org/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost
-    if ('string' === typeof urlString) {
-      // check is an uri
+function parseUriParts(urlString: string): UrlWithStringQuery{
+    // initialize return value
+    const returnValue : UrlWithStringQuery = uriParse('');
+    if(urlString){
+      // A TypeError is thrown if urlString is not a string
+      // @see https://nodejs.org/api/url.html#url_url_parse_urlstring_parsequerystring_slashesdenotehost 
+      if('string' === typeof urlString){
+        // check is an uri
+        // return uriParse(urlString) // take apart the uri
+      }
       return uriParse(urlString) // take apart the uri
     }
   }
