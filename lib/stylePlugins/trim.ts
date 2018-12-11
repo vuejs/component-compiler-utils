73aeec6aca0bd11b8e440fcd6b49a9af7f663c1c
Alexander Lichter <manniL@gmx.net>
2018-12-11 09:59:46 +0000

    fix(trim): don't insert line break

--- lib/stylePlugins/trim.ts
+++ lib/stylePlugins/trim.ts
@@ -4,7 +4,7 @@ import * as postcss from 'postcss'
 export default postcss.plugin('trim', () => (css: Root) => {
   css.walk(({ type, raws }) => {
     if (type === 'rule' || type === 'atrule') {
-      raws.before = raws.after = '\n'
+      raws.before = raws.after = ''
     }
   })
 })
