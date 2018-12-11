2603ee238a6c8114094a1339a5060c629c0b12e0
Evan You <yyx990803@gmail.com>
2018-12-11 12:40:24 -0500

    fix: do not insert newline if style is already minified

--- lib/stylePlugins/trim.ts
+++ lib/stylePlugins/trim.ts
@@ -4,7 +4,8 @@ import * as postcss from 'postcss'
 export default postcss.plugin('trim', () => (css: Root) => {
   css.walk(({ type, raws }) => {
     if (type === 'rule' || type === 'atrule') {
-      raws.before = raws.after = '\n'
+      if (raws.before) raws.before = '\n'
+      if (raws.after) raws.after = '\n'
     }
   })
 })
